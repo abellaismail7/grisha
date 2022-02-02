13664d0bd2ea145128e1d020e1df2170471dba19
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-02-02 21:05:19 +0000

    Hello world!

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,24 @@
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
+// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {
-
-    }
+    loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn start() -> ! {
-    loop {
+static HELLO: &[u8] = b"Long live THE KING";
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x2;
+        }
     }
 
+    loop {}
 }
