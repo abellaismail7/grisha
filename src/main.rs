11815faf796512a1d401d5b807d3184e49ec0b93
Ismail A Bella <60820321+abellaismail7@users.noreply.github.com>
2022-02-02 15:21:07 +0000

    abort on panic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
